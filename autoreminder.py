eb604191b8362b52bef33b540bbab05700eeac60
Alan Wang <22726288+alanwangcot@users.noreply.github.com>
2022-09-06 15:18:35 +0800

    Add files via upload

--- autoreminder.py
+++ autoreminder.py
@@ -9,7 +9,7 @@ token = requests.get(tokenurl).json()["access_token"]
 
 wxuserdebug = "oHWB75go3M5mAOuQ6ShTOjOyDCNg"
 wxuser = "oHWB75vv2Qr73u48SDQcLaAGPPUo"
-template_id = "3zp6LEP1az7HvhLw_ykP0cyNwYz-gkgfeSDAaGozMFo"
+template_id = "EjK-oxpO8tnWEcZDc6bg-HJ-b684DQH3i2viwAzeFNk"
 
 
 warn_url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=tc"
@@ -17,6 +17,7 @@ response = requests.get(warn_url)
 response.encoding = 'utf-8'
 json_data = response.json()
 warning = "没有警告哦"
+print("got warning")
 
 if 'WTCSGNL' in json_data:
     warning = "有風球警告！" + json_data['WTCSGNL']['type']
@@ -47,6 +48,7 @@ response = requests.get(wea_url)
 response.encoding = 'utf-8'
 json_data = response.json()
 forecast = json_data['forecastDesc']
+print("got forecast msg")
 
 temp_url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc"
 temp_response = requests.get(temp_url)
@@ -55,59 +57,19 @@ temp_json_data = temp_response.json()
 max = temp_json_data['weatherForecast'][0]['forecastMaxtemp']['value']
 min = temp_json_data['weatherForecast'][0]['forecastMintemp']['value']
 temp_str = "今日溫度" + str(min) + "度和" + str(max) + "度之間。"
+print("got temp msg")
 
-startdate = date(2022,9,5)
+startdate = date(2022,8,22)
 enddate = date.today()
 days = (enddate - startdate).days
 
-# joke_url = "https://v2.alapi.cn/api/joke/random"
+joke_url = "https://api.vvhan.com/api/joke?type=json"
 # headers = {'Content-Type': "application/x-www-form-urlencoded"}
 # joke_response = requests.request("POST", joke_url, data="token=0e4TW86CjLPwmkSy", headers = headers)
-# joke_data = joke_response.json()
-
-# peom_token = "1PtlywpoukHYcEODbBbnyFLuEdFW3JOG"
-# poem_headers = {'X-User-Token': peom_token}
-poem_url = "https://v1.jinrishici.com/shuqing/sinian"
-poem_response = requests.get(poem_url)
-poem_data = poem_response.json()
-
-
-
-jokes = [
-    "我说：“你是猪。”你说：“我是猪才怪!”从此我就叫你猪才怪。终于有一天，你忍不住当着众人的面对我吼道：“我不是猪才怪!",
-    "都说海底捞服务好，不是没道理，我上次吃了霸王餐，服务员亲自报警。警察来了还给警察递热毛巾，走的时候还给手铐上挂一袋妙脆角。",
-    "有个少年立志要当上海贼王，于是上海人民纷纷锁紧了房门。",
-    "从前有一只大鱼和一只小鱼。有一天，小鱼问大鱼：大～鱼～大～鱼～你～平～常～喜～欢～吃～什～么～丫。大鱼说：我～喜～欢～吃～说～话～慢～的～小～鱼。然后小鱼说：喔，酱紫，造了！",
-
-    "冬天，农夫在路上捡到一条冬眠的蛇，就把它放在怀里带回了家。到家了蛇还没醒，！农夫就把它放到了酒罐子里准备泡酒喝。第二年春天，蛇从冬眠里醒来，从酒罐子里爬出来说的第一句话就是：“我他妈也是醉了。",
-    '看到没，那棵树快要枯了\n那我过去抱抱它',
-    "她生气了，夺门而出，他冲到楼下拦住她，把门夺了回来",
-    "前几天去医院体检了，好开心，仅仅只花了200块钱就检查出六个病来。",
-    "诸葛亮:“风啊，你向西刮”风:“你才像西瓜”！！！",
-    "开车路过一个小泥潭，小泥潭溅的水好响，原来是好响泥。",
-    
-    "那天家里卧室隔壁的灯一闪一闪的，叫来维修师傅，师傅问什么问题？我说：“卧室隔壁灯太闪”他：“抓住爱情的藤蔓？",
-    "甲:你有《时间简史》吗?乙:神经病，我有时间捡那玩意儿干啥?",
-    "从前有只麋鹿，它在森林里玩儿，不小心走丢了。于是它给它的好朋友长颈鹿打电话：“喂…我迷路啦。”长颈鹿听见了回答说：“喂，我长颈鹿啦~ ",
-    "在商场一个婴儿一直在哭搞得我很烦，朋友劝我要换位思考。现在我坐在婴儿车里，还是很烦。",
-        "这是拿什么肉煲的汤？母鸡啦",
-
-]
-# weather_msg = weather_data["wea"] + "，温度" + weather_data["tem2"] + "~" + weather_data["tem1"] + "度，空气湿度" + weather_data["humidity"] + "，还有" + weather_data["win_speed"] + "风。"
-# if weather_data["wea_img"] == "qing" or weather_data["wea_img"] == "yun":
-#     weather_msg += "天气不错，要有好心情哦！"
-
-# if weather_data["wea_img"] == "xue" or weather_data["wea_img"] == "yu" or weather_data["wea_img"] == "lei":
-#     weather_msg += "下雨天，要记得带伞呀！"
-# if weather_data["wea_img"] == "shachen" or weather_data["wea_img"] == "wu" or weather_data["wea_img"] == "yin":
-#     weather_msg += "没有太阳，适合宅家！"
-# if weather_data["wea_img"] == "bingbao":
-#     weather_data += "什么鬼天气，竟然有冰雹，快在家里躲着吧！"
-
-if days >= 0:
-    joke_msg = jokes[days]
-else:
-    joke_msg = "冇更多笑话啦！不如同我倾计呀，我来撩你笑:)"
+joke_response = requests.get(joke_url)
+joke_data = joke_response.json()
+print("got joke")
+joke_msg = joke_data["joke"]
 
 data_debug = {
     "touser": wxuserdebug,
@@ -126,7 +88,7 @@ data_debug = {
             "value": warning + "！",
             "color": "#520"
         },
-        "daysleftMsg": {
+        "daysAgoMsg": {
             "value": str(days + 1),
             "color": "#173177"
         },
@@ -172,16 +134,13 @@ data = {
     }
 }
 
-def send_msg(send_data):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + token
-    response = requests.post(url, data=json.dumps(send_data))
-    with open("log.json", 'a') as f:
-        f.write(enddate.strftime("%Y年%m月%d日"))
-        f.write(response.text)
-
-    return response.json
-
-
-
-if __name__ == '__main__':
-    send_msg(data_debug)
+# print(data)
+url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + token
+response1 = requests.post(url, data=json.dumps(data_debug))
+print("message sent to API!")
+# response2 = requests.post(url, data=json.dumps(data))
+with open('log.json', 'w') as f:
+    f.write(startdate.strftime("%Y年%m月%d日"))
+    f.write(str(response1.json()))
+    # f.write(str(response2.json()))
+    # f.close()
